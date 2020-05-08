f29721aa0155879cb0bc4a684f40f11ef86f998f
kkuop <60794860+kkuop@users.noreply.github.com>
2020-05-08 14:15:05 -0500

    adding categories for the flash cards

--- src/index.js
+++ src/index.js
@@ -7,9 +7,48 @@ class App extends React.Component {
     render() {
         return (
             <div className="container-fluid">
+                <div className="row justify-content-center">
+                    <div className="col-sm-8">
+                        <Categories />
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-sm-8">
+
+                    </div>
+                </div>
                 <Button variant="primary" className="mr-2">Bootstrap!</Button>
             </div>
             
+        );
+    }
+}
+
+class Categories extends React.Component {
+    render() {
+        return (
+            <div className="row">
+            <div className="col-sm">
+                <Category />
+            </div>
+        </div>
+        );
+
+    }
+}
+
+class Category extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-sm">
+                    <div className="fc-card">C#</div>
+                    <div className="fc-card">SQL</div>
+                    <div className="fc-card">HTML</div>
+                    <div className="fc-card">CSS</div>
+                    <div className="fc-card">JavaScript</div>
+                </div>
+            </div>
         );
     }
 }
