67daeb87814db9147fc09cf4c6d2de7dd2271a39
kkuop <60794860+kkuop@users.noreply.github.com>
2020-05-08 22:08:30 -0500

    centering the categories and adding the main card

--- src/index.js
+++ src/index.js
@@ -14,12 +14,25 @@ class App extends React.Component {
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-sm-8">
-
+                        <MainCard />
                     </div>
                 </div>
+                <div className="row justify-content-center">
+                    <div className="">
+
+                    </div>
+                </div> 
                 <Button variant="primary" className="mr-2">Bootstrap!</Button>
             </div>
             
+        );
+    }
+}
+
+class MainCard extends React.Component {
+    render() {
+        return (
+            <div></div>
         );
     }
 }
@@ -28,25 +41,24 @@ class Categories extends React.Component {
     render() {
         return (
             <div className="row">
-            <div className="col-sm">
-                <Category />
+            <div className="col-sm text-center">
+                <Category value="C#"/>
+                <Category value="SQL"/>
+                <Category value="HTML"/>
+                <Category value="CSS"/>
+                <Category value="JavaScript"/>
             </div>
         </div>
         );
-
     }
 }
 
 class Category extends React.Component {
     render() {
         return (
-            <div className="row">
-                <div className="col-sm">
-                    <div className="fc-card">C#</div>
-                    <div className="fc-card">SQL</div>
-                    <div className="fc-card">HTML</div>
-                    <div className="fc-card">CSS</div>
-                    <div className="fc-card">JavaScript</div>
+            <div className="row fc-cards">
+                <div className="col-sm text-center">
+                    <div className="fc-card">{this.props.value}</div>
                 </div>
             </div>
         );
