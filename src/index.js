41e4f7390375d8a48e7febf265074ae871c8e85b
kkuop <60794860+kkuop@users.noreply.github.com>
2020-05-15 21:10:27 -0500

    Added test json server file... added categorylist component which will map category components and pass in the names

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {Button} from 'react-bootstrap';
 
+const SAMPLE = [
+    {
+        id:1,
+        question: 'A named space in memory',
+        answer: 'Variable'
+    },
+    {
+        id:2,
+        question: 'Template for an object that consists of member variables, constructor, methods',
+        answer: 'Class'
+    }
+]
+
 class App extends React.Component {
     render() {
         return (
@@ -18,37 +31,56 @@ class App extends React.Component {
                     </div>
                 </div>
                 <div className="row justify-content-center">
-                    <div className="">
-
+                    <div className="col-sm text-center">
+                        <Button variant="primary" className="mr-2 fc-btn">NEXT</Button>
                     </div>
-                </div> 
-                <Button variant="primary" className="mr-2">Bootstrap!</Button>
-            </div>
-            
+                </div>
+            </div>            
         );
     }
 }
 
 class MainCard extends React.Component {
+    state = {
+        flashcards : SAMPLE
+    }
+    componentDidMount() {
+
+    }
     render() {
         return (
-            <div></div>
+            <div className="row justify-content-center">
+                <div className="col-sm-5">
+                    <div className="main-card">
+                        <div className="content-main">{this.state.flashcards[0].question}</div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
 
 class Categories extends React.Component {
+    state = {
+        values: ['C#','SQL','HTML','CSS','JavaScript']
+    }
     render() {
         return (
             <div className="row">
-            <div className="col-sm text-center">
-                <Category value="C#"/>
-                <Category value="SQL"/>
-                <Category value="HTML"/>
-                <Category value="CSS"/>
-                <Category value="JavaScript"/>
+                <div className="col-sm text-center">
+                    <CategoryList categories = {this.state.values}/>
+                </div>
             </div>
-        </div>
+        );
+    }
+}
+
+class CategoryList extends React.Component {
+    render() {
+        return (
+            this.props.categories.map(cat => {
+                return <Category name = {cat}/>
+            })
         );
     }
 }
@@ -58,12 +90,14 @@ class Category extends React.Component {
         return (
             <div className="row fc-cards">
                 <div className="col-sm text-center">
-                    <div className="fc-card">{this.props.value}</div>
+                    <div className="fc-card">{this.props.name}</div>
                 </div>
             </div>
         );
     }
 }
+
+
 
 ReactDOM.render(
     <App />,
